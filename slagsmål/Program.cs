ecb832b273d19ca34616f0acc3355da2718cb355
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2023-09-21 10:54:26 +0200

    ll

--- slagsmål/Program.cs
+++ slagsmål/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿//slump
+
+/*Random generator = new Random();
+
+int slumptal = generator.Next(10, 20);
+
+Console.WriteLine(slumptal);
+*/
+//while-loop
+
+int tal = 100;
+
+while (tal > 0){
+    Console.WriteLine(tal);
+    tal--; 
+}
+
+Console.WriteLine("färdig")
+
+Console.ReadLine();
