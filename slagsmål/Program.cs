bec126f317ca6fc3ddea5bd39d8c7eeccb023b8a
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2023-09-26 10:53:19 +0200

    kk

--- slagsmål/Program.cs
+++ slagsmål/Program.cs
@@ -4,14 +4,25 @@ int hp2 = 500;
 int dmg = 0;
 int dmg2 = 0;
 
+string name = "";
+
+Console.ReadLine();
+
+Console.WriteLine($"welcome {name} you'll be going up against villan");
+
+Console.ReadLine();
+
 Random generator = new Random();
+Console.WriteLine("Player A mot player B båda har 500 hp");
+Console.WriteLine("Samt gör dmg mellan 0-250");
+
+Console.ReadLine();
 
 if (hp > 0){
    
 int dmg = generator.Next(0, 250);
 
-hp-dmg;
-
+hp-= dmg;
 }
 
 else 
@@ -22,7 +33,7 @@ Console.WriteLine("PLAYER 1 VANN")
 
 
 if (hp2 > 0){
-   Console.WriteLine("")
+   Console.WriteLine("du har "hp2"hp kvar");
 int dmg2 = generator.Next(0, 250);
 hp2-dmg2;
 }
