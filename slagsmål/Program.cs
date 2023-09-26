c559e6c1d992034b23b0aa832e3c85b23e17f78d
Mikael Edvardsson <Mikael.Edvardsson@elev.ga.ntig.se>
2023-09-26 10:30:14 +0200

    kk

--- slagsmål/Program.cs
+++ slagsmål/Program.cs
@@ -1,19 +1,37 @@
-﻿//slump
+﻿
+int hp = 500;
+int hp2 = 500;
+int dmg = 0;
+int dmg2 = 0;
 
-/*Random generator = new Random();
+Random generator = new Random();
 
-int slumptal = generator.Next(10, 20);
+if (hp > 0){
+   
+int dmg = generator.Next(0, 250);
 
-Console.WriteLine(slumptal);
-*/
-//while-loop
+hp-dmg;
 
-int tal = 100;
-
-while (tal > 0){
-    Console.WriteLine(tal);
-    tal--; 
 }
+
+else 
+{
+   
+Console.WriteLine("PLAYER 1 VANN")
+}
+
+
+if (hp2 > 0){
+   Console.WriteLine("")
+int dmg2 = generator.Next(0, 250);
+hp2-dmg2;
+}
+else 
+{
+   
+Console.WriteLine("PLAYER 2 VANN")
+}
+
 
 Console.WriteLine("färdig")
 
